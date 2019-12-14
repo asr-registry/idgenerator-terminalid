63fd1322c2225867d559feb65d436e15f1742c72
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-14 10:27:26 +0430

    cmm exceptions

---
+++ src/main/java/af/asr/terminalid/IdgeneratorTerminalidApplication.java
@@ -0,0 +1,13 @@
+package af.asr.terminalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdgeneratorTerminalidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdgeneratorTerminalidApplication.class, args);
+	}
+
+}
---
+++ src/main/java/af/asr/terminalid/exception/crypto/exception/NullMethodException.java
@@ -0,0 +1,36 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+package af.asr.terminalid.exception.crypto.exception;
+
+
+import af.asr.cryptosignature.exception.common.BaseUncheckedException;
+
+/**
+ * {@link Exception} to be thrown when key is null
+ */
+public class NullMethodException extends BaseUncheckedException {
+
+	/**
+	 * Unique id for serialization
+	 */
+	private static final long serialVersionUID = -7652091398077351394L;
+
+	/**
+	 * Constructor with errorCode and errorMessage
+	 * 
+	 * @param errorCode    The error code for this exception
+	 * @param errorMessage The error message for this exception
+	 */
+
+	public NullMethodException(String errorCode, String errorMessage) {
+		super(errorCode, errorMessage);
+	}
+
+}
